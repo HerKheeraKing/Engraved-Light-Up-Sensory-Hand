44e8c6121ba6634cda65e71aa009273b015849b0
Joshua F <jskrillex6@gmail.com>
2025-03-12 03:13:09 -0400

    Small fixes to help stablize library/faster responses

--- Unity Projects/PanelManager/Assets/Scripts/BluetoothManager.cs
+++ Unity Projects/PanelManager/Assets/Scripts/BluetoothManager.cs
@@ -50,7 +50,7 @@ public class BluetoothManager : MonoBehaviour {
     }
 
     IEnumerator DelayedConnected() {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(3.0f);
         UserData user = isDefaultUser ? UserManager.Instance.users.allUsers[0] : curArduinoUser;
         if (isDefaultUser)
             SetUser(user);
