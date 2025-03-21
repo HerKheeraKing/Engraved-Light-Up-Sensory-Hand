6394268fda64b171f933b5214ab8c1d7a0287555
Joshua F <jskrillex6@gmail.com>
2025-03-21 12:43:58 -0400

    Updated light panel to be smaller and without ridges

--- Arduino/BacklitPanel/BacklitPanel.ino
+++ Arduino/BacklitPanel/BacklitPanel.ino
@@ -2,17 +2,16 @@
 #include <PanelManager.h>
 
 #define LEDPIN 12
-#define LEDCOUNT 10//20
-#define REDPOT 32
-#define GRNPOT 33
-#define BLUPOT A2 //34
-#define TOTPOT A4 //36
+#define LEDCOUNT 20
+#define REDPOT A2
+#define GRNPOT A4
+#define BLUPOT 33
+#define TOTPOT 32
 
 Adafruit_NeoPixel strip(LEDCOUNT, LEDPIN, NEO_GRBW + NEO_KHZ800);
 PanelManager panelManager("Panel-Backlit");
 
 void setup() {
-  Serial.begin(115200);
   panelManager.InitManager();
   analogSetPinAttenuation(REDPOT, ADC_11db);
   analogSetPinAttenuation(GRNPOT, ADC_11db);
@@ -27,10 +26,6 @@ void loop() {
   int r = constrain((analogRead(REDPOT) / 16) * t / 255, 0, 255);
   int g = constrain((analogRead(GRNPOT) / 16) * t / 255, 0, 255);
   int b = constrain((analogRead(BLUPOT) / 16) * t / 255, 0, 255);
-  Serial.print("R: "); Serial.print(r);
-  Serial.print(" | G: "); Serial.print(g);
-  Serial.print(" | B: "); Serial.print(b);
-  Serial.print(" | T: "); Serial.println(t);
   SetColor(r, g, b);
   delay(100);
 }
